77aca81c8ee8ab6746a3ee7df54fa9938927ce1d
ihyeung <yeung.ireneh@gmail.com>
2019-01-02 00:21:37 -0700

    displaying recent chat messages between two users when clicking on the item in the messaging segment of messaging tab, sending a message is successful at hitting database and displaying to screen

--- src/pages/inbox/inbox.ts
+++ src/pages/inbox/inbox.ts
@@ -9,6 +9,7 @@ import { Utilities, Messages, Matches, GlobalVars } from '../../providers';
 })
 export class MessageInboxPage {
 
+  inboxSelector: string = 'matches';
   matchProfileId: number;
   inboxMessagePreviews: any = []; //The latest PupperMessage exchanged between user and all of their matches
   inboxMessageRecentHistory: any = []; //The 5 most recent PupperMessages exchanged between user and all of their matches
@@ -20,15 +21,19 @@ export class MessageInboxPage {
   constructor(public navCtrl: NavController, public globalVars: GlobalVars,
     public matchService: Matches, public msgService: Messages,
     public utilService: Utilities) {
-      if (null == this.globalVars.getMatchProfileObj()) {
-        this.noMatchProfileFoundHandler();
-      }
-      this.matchProfileId = this.globalVars.getMatchProfileObj()['id'];
-      this.retrieveMatches(); //Default to show contents of matches tab
+
+    }
+
+    ionViewDidLoad() {
+      this.utilService.getDataFromStorage('match').then(val => {
+        this.matchProfileId = val['id'];
+
+        this.retrieveMatches(); //Default view to show contents of matches tab
+      });
     }
 
     retrieveMatches() {
-      if (this.matchesReady) {
+      if (this.matchesReady) { //Only load once
         return;
       }
       this.matchService.getMatchesByMatchProfileId(this.matchProfileId)
@@ -44,73 +49,66 @@ export class MessageInboxPage {
           });
         });
         this.matchesReady = true;
+        console.log('Matches are now ready');
       }, err => console.error('ERROR', err));
-   
+
+    }
+
+    retrieveMessageHistoriesWithMatches() {
+      if (this.messagesReady) {
+        return;
+      }
+      this.msgService.retrieveMessagesForInbox(this.matchProfileId)
+      .subscribe(response => {
+        const messageHistoryList = JSON.parse(response['_body']);
+        messageHistoryList.forEach(history => {
+          if (history === undefined || history.length == 0) {
+            // console.log('No messages exchanged between these users yet');
+          } else {
+            let otherMatchProfile = history[0]['matchProfileSender'];
+            let incomingMessage = true;
+            if ( otherMatchProfile['id'] == this.matchProfileId) { //Make sure to add the other user's id and name to message array
+            otherMatchProfile = history[0]['matchProfileReceiver'];
+            incomingMessage = false;
+          }
+
+          this.inboxMessageRecentHistory.push(history);
+          this.inboxMessagePreviews.push({
+            matchProfileObj: otherMatchProfile,
+            matchProfileName: otherMatchProfile['names'],
+            image: otherMatchProfile['profileImage'] ? otherMatchProfile['profileImage'] : 'assets/img/appLogo.png',
+            message: history[0]['message'],
+            timestamp: history[0]['timestamp'],
+            incomingMessage: incomingMessage
+          });
+        }
+      });
+      this.messagesReady = true;
+    }, err => console.error('ERROR', err));
   }
 
-  retrieveMessageHistoriesWithMatches() {
-    if (this.messagesReady) {
-      return;
-    }
-    this.msgService.retrieveMessagesForInbox(this.matchProfileId)
-    .subscribe(response => {
-      // console.log('Response body: ' + response['_body']);
-      const messageHistoryList = JSON.parse(response['_body']);
-      messageHistoryList.forEach(history => {
-        if (history === undefined || history.length == 0) {
-          console.log('No messages exchanged between these users yet');
-        } else {
-          let otherMatchProfileId = history[0]['matchProfileSender']['id'];
-          let otherMatchProfileName = history[0]['matchProfileSender']['names'];
-          let otherMatchProfileImage = history[0]['matchProfileSender']['profileImage'];
-
-          if ( otherMatchProfileId == this.matchProfileId) { //Make sure to add the other user's id and name to message array
-          otherMatchProfileId = history[0]['matchProfileReceiver']['id'];
-          otherMatchProfileName = history[0]['matchProfileReceiver']['names'];
-          otherMatchProfileImage = history[0]['matchProfileReceiver']['profileImage'];
-        }
-        this.inboxMessageRecentHistory.push(history);
-        this.inboxMessagePreviews.push({
-          matchProfileId: otherMatchProfileId,
-          matchProfileName: otherMatchProfileName,
-          image: otherMatchProfileImage == null ? 'assets/img/appLogo.png' : otherMatchProfileImage,
-          message: history[0]['message'],
-          timestamp: history[0]['timestamp']
-        });
-      }
+  viewMatchProfile(matchListEntry) {
+    const matchProfileId = matchListEntry['matchProfileObj']['id'];
+    console.log('clicked on match profile for matchProfileId=' + matchProfileId);
+    this.navCtrl.push('MatchProfileDetailPage', {
+      matchProfileId: matchProfileId
     });
-    this.messagesReady = true;
-  }, err => console.error('ERROR', err));
-}
+  }
 
-viewMatchProfile(matchListEntry) {
-  const matchProfileId = matchListEntry['matchProfileId'];
-  console.log('clicked on match profile for matchProfileId=' + matchProfileId);
-}
+  viewMessageHistory(inboxPreviewItem) {
+    const matchProfile = inboxPreviewItem['matchProfileObj'];
+    console.log('viewMessageHistory matchProfileObj: ' + matchProfile);
+    const matchProfileId = matchProfile['id'];
+    console.log('clicked to view message history for current user & matchProfileId=' + matchProfileId);
 
-viewMessageHistory(inboxPreviewItem) {
-  const matchProfileId = inboxPreviewItem['matchProfileId'];
-  console.log('clicked to view message history for current user & matchProfileId=' + matchProfileId);
+    const listIndex = this.inboxMessagePreviews.indexOf(inboxPreviewItem);
 
-  const listIndex = this.inboxMessagePreviews.indexOf(inboxPreviewItem);
+    console.log('Retrieving message history for inboxMessagePreviews array element ' + listIndex);
 
-  console.log('Retrieving message history for inboxMessagePreviews array element ' + listIndex);
-
-  this.navCtrl.push('ChatPage', this.inboxMessageRecentHistory[listIndex]);
-}
-
-
-noMatchProfileFoundHandler() {
-  const createMatchProfile = this.utilService.displayAlertDialog(
-    'Create a matching profile?',
-    'Please create a matching profile to begin matching.',
-    'Cancel',
-    'Create match profile');
-    if (createMatchProfile) {
-      this.navCtrl.push('CreateMatchProfilePage');
-    } else {
-      this.navCtrl.push('ProfileSnapshotPage');
-    }
+    this.navCtrl.push('ChatPage', {
+      matchProfileReceiver: matchProfile,
+      messages: this.inboxMessageRecentHistory[listIndex]
+    });
   }
 
 }
