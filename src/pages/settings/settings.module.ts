442d98882acb5131562f7f462146637e86ffe9e8
ihyeung <yeung.ireneh@gmail.com>
2019-01-02 00:19:53 -0700

    prompt dialog for logout and account deletion, ui

--- src/pages/settings/settings.module.ts
+++ src/pages/settings/settings.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SettingsPage } from './settings';
+import { Dialogs } from '@ionic-native/dialogs';
 
 @NgModule({
   declarations: [
     SettingsPage,
   ],
   imports: [
-    IonicPageModule.forChild(SettingsPage),
+    IonicPageModule.forChild(SettingsPage)
   ],
   exports: [
     SettingsPage
-  ]
+  ],
+  providers: [ Dialogs]
 })
 export class SettingsPageModule {}
--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, App, IonicPage } from 'ionic-angular';
+import { NavController, App, IonicPage, AlertController } from 'ionic-angular';
 import { environment as ENV } from '../../environments/environment';
-import { GlobalVars, Utilities } from '../../providers';
-import { Storage } from '@ionic/storage';
-// import { Users } from '../../providers/http/userProfiles';
-// import { Matches } from '../../providers/http/matches';
-// import { Messages } from '../../providers/http/messages';
-// import { MatchProfiles } from '../../providers/http/matchProfiles';
+import { Utilities } from '../../providers';
+import { Dialogs } from '@ionic-native/dialogs';
 
 @IonicPage()
 @Component({
@@ -15,60 +11,79 @@ import { Storage } from '@ionic/storage';
 })
 export class SettingsPage {
 
+  matchProfileObj: any = [];
+  userObj: any = [];
+
   constructor(public navCtrl: NavController, public app: App,
-    public globalVars: GlobalVars, public utilService: Utilities,
-    private storage: Storage) {
-    // public userService: Users, public matchProfService: MatchProfiles,
-    // public matchService: Matches, public msgService: Messages) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SettingsPage');
-  }
-
-  userProfile() {
-
-  }
-
-  matchProfile() {
-
-  }
-
-  logout() {
-    const confirm = this.utilService.displayAlertDialog('Log out?',
-    'Are you sure you want to log out?', 'Cancel','Confirm');
-    // if (confirm) {
-      console.log('logging out');
-      this.storage.clear();
-      this.returnToHomeScreen();
-    // }
-
-  }
-
-  deleteAccount() {
-    const confirmDelete = this.utilService.displayAlertDialog('Delete your account?',
-    'Are you sure you want to delete your account? This action cannot be undone.',
-    'Cancel','Delete my account');
-    if (confirmDelete) {
-      this.deleteAllUserData();
-      this.storage.clear();
-      this.returnToHomeScreen();
+    public alertCtrl: AlertController,
+    private utilService: Utilities, private dialog: Dialogs) {
+      // public userService: Users, public matchProfService: MatchProfiles,
+      // public matchService: Matches, public msgService: Messages) {
     }
-  }
 
-  deleteAllUserData() {
-    //Required delete order (due to foreign key constraints):
-    //1. delete all messages
-    //1. delete match results
-    //2. delete pupper profiles (if implemented)
-    //3. delete match Profiles
-    //4. delete user profile
-    //5. delete user account
-  }
+    ionViewDidLoad() {
+      console.log('ionViewDidLoad SettingsPage');
+      this.utilService.getDataFromStorage('match').then(val => {
+        this.matchProfileObj = val;
+        this.userObj = val['userProfile'];
+      });
+    }
 
-  returnToHomeScreen() {
-    this.app.getRootNav().setRoot('HomePage');
-    this.app.getRootNav().popToRoot();
-  }
+    userProfile() {
+      this.dialog.alert('User Profile button clicked')
+      .then(() => console.log('Dialog dismissed'))
+      .catch(e => console.log('Error displaying dialog', e));
+    }
 
-}
+    matchProfile() {
+
+    }
+
+    logout() {
+      this.dialog.confirm('Are you sure you want to log out?', 'Log out')
+      .then(index => {
+        if (index == 1) {
+          this.utilService.clearStorage();
+          this.returnToHomeScreen();
+        }
+      })
+      .catch(e => console.log('Error displaying dialog', e));
+
+    }
+
+    deleteAccount() {
+      this.dialog.prompt(
+        'Are you sure you want to permanently delete your account and all associated user data? This action cannot be undone.',
+        'Delete Account', ['Confirm', 'Cancel'], 'Enter your email to confirm.')
+      .then(obj => {
+        if (obj.buttonIndex != 1) {
+          return;
+        }
+          if (obj.input1 === undefined || obj.input1 != this.userObj['userAccount']['username']) {
+            console.log('does not match');
+          } else {
+            console.log('deleting account');
+            this.deleteAllUserData();
+            this.utilService.clearStorage();
+            this.returnToHomeScreen();
+          }
+      })
+      .catch(e => console.log('Error displaying dialog', e));
+    }
+
+    deleteAllUserData() {
+      //Required delete order (due to foreign key constraints):
+      //1. delete all messages
+      //1. delete match results
+      //2. delete pupper profiles (if implemented)
+      //3. delete match Profiles
+      //4. delete user profile
+      //5. delete user account
+    }
+
+    returnToHomeScreen() {
+      this.app.getRootNav().setRoot('HomePage');
+      // this.app.getRootNav().popToRoot();
+    }
+
+  }
