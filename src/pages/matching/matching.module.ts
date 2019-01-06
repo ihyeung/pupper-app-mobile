39046e2710737562f197c490e0841c768cb8ce14
ihyeung <yeung.ireneh@gmail.com>
2019-01-06 01:23:13 -0700

    added necessary changes to confirm that images that arent url shortened are now loading on profile cards, null image fields for newly created match profiles get replaced by default img, now successfully retrieving match profiles in batches so when one card is left, http call is made to retrieve more

--- src/pages/matching/matching.module.ts
+++ src/pages/matching/matching.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { MatchingPage } from './matching';
 import { SwipeCardsModule } from 'ng2-swipe-cards';
-// import { DomSanitizer } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
-    MatchingPage,
+    MatchingPage
   ],
   imports: [
     IonicPageModule.forChild(MatchingPage),
