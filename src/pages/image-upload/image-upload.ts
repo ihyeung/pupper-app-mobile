4df476acc9e9248fd3b9581235c8a14cbb9b1004
ihyeung <yeung.ireneh@gmail.com>
2019-01-08 01:05:10 -0700

    Filepath and filename for image selected from library working, but image file preview not being displayed, copying to local directory confirmed working on ios, camera not testable

--- src/pages/image-upload/image-upload.ts
+++ src/pages/image-upload/image-upload.ts
@@ -16,9 +16,11 @@ export class ImageUploadPage {
   lastImage: string = null;
   imageFor: string;
   profileData: any = [];
-  imageURI: string = '';
+  imageURI: string;
+  imageTemp: string;
   imageFileName: any;
   uriReady: boolean = false;
+  tempReady: boolean = false;
 
 
 
@@ -37,50 +39,62 @@ export class ImageUploadPage {
       console.log('ionViewDidLoad ImageUploadPage');
     }
 
-  public selectImageForUpload() {
-    const actionSheet = this.actionSheetCtrl.create({
-      title: 'Select Image Source',
-      buttons: [
-        {
-          text: 'Load from Library',
-          handler: () => {
-            this.selectExistingOrTakePhoto(this.camera.PictureSourceType.PHOTOLIBRARY);
+    public selectImageForUpload() {
+      const actionSheet = this.actionSheetCtrl.create({
+        title: 'Select Image Source',
+        buttons: [
+          {
+            text: 'Load from Library',
+            handler: () => {
+              this.selectExistingOrTakePhoto(this.camera.PictureSourceType.PHOTOLIBRARY);
+            }
+          },
+          {
+            text: 'Use Camera',
+            handler: () => {
+              this.selectExistingOrTakePhoto(this.camera.PictureSourceType.CAMERA);
+            }
+          },
+          {
+            text: 'Cancel',
+            role: 'cancel'
           }
-        },
-        {
-          text: 'Use Camera',
-          handler: () => {
-            this.selectExistingOrTakePhoto(this.camera.PictureSourceType.CAMERA);
-          }
-        },
-        {
-          text: 'Cancel',
-          role: 'cancel'
-        }
-      ]
-    });
-    actionSheet.present();
-  }
+        ]
+      });
+      actionSheet.present();
+    }
 
-  public selectExistingOrTakePhoto(sourceType) {
-    const cameraDialogOptions = {
-      quality: 100,
-      sourceType: sourceType,
-      saveToPhotoAlbum: true,
-      correctOrientation: true,
-      destinationType: this.camera.DestinationType.FILE_URI
+    private setCameraOptions(sourceType) {
+      const options: CameraOptions = {
+        quality: 100,
+        sourceType: sourceType,
+        // saveToPhotoAlbum: true,
+        correctOrientation: true,
+        destinationType: this.camera.DestinationType.FILE_URI,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE
+      };
+      return options;
+    }
 
-    };
+    public selectExistingOrTakePhoto(sourceType) {
+      // const cameraDialogOptions : CameraOptions = {
+      //   quality: 100,
+      //   sourceType: sourceType,
+      //   // saveToPhotoAlbum: true,
+      //   correctOrientation: true,
+      //   destinationType: this.camera.DestinationType.FILE_URI,
+      //   encodingType: this.camera.EncodingType.JPEG,
+      //   mediaType: this.camera.MediaType.PICTURE
+      //   // destinationType: this.camera.DestinationType.FILE_URI
+      //
+      // };
+      const cameraDialogOptions = this.setCameraOptions(sourceType);
 
-    this.camera.getPicture(cameraDialogOptions).then((imagePath) => {
-      this.imageURI = imagePath;
-      this.uriReady = true;
-
-      console.log(imagePath);
-
-      if (this.platform.is('android') && //If android and select an existing photo
-      sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
-        this.filePath.resolveNativePath(imagePath)
+      this.camera.getPicture(cameraDialogOptions).then((imagePath) => {
+        if (this.platform.is('android') && //If android and select an existing photo
+        sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
+          this.filePath.resolveNativePath(imagePath)
           .then(filePath => {
             console.log('file path: ', filePath);
 
@@ -91,51 +105,54 @@ export class ImageUploadPage {
 
             this.copyFileToLocalDir(path, name);
           });
+        }
+        else if (this.platform.is('android') && //If android and take a photo with camera
+        sourceType === this.camera.PictureSourceType.CAMERA) {
+          console.log("Android platform and photo taken with camera");
+        } else { //ios
+          console.log("iOS platform");
+          const name = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+          const path = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+          console.log('Path from filePath substring: ' + path);
+          console.log('Name from imagePath substring: ' + name);
+          this.copyFileToLocalDir(path, name);
+        }
+      }, (err) => console.log(err));
+    }
+
+    private copyFileToLocalDir(namePath, currentName) {
+      const imageFileName = new Date().getTime() + '.jpg';
+      this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, imageFileName)
+      .then(success => {
+        this.lastImage = imageFileName;
+        this.imageURI = this.pathForImage(this.lastImage);
+        this.uriReady = true;
+
+      }, err => console.error('ERROR', JSON.stringify(err)));
+    }
+
+    public pathForImage(img) {
+      const path = img === null ? '' : cordova.file.dataDirectory + img;
+
+      if (img !== null) {
+        console.log("pathForImage() function, path is " + path);
       }
-      else if (this.platform.is('android') && //If android and take a photo with camera
-      sourceType === this.camera.PictureSourceType.CAMERA) {
-        console.log("Android platform and photo taken with camera");
-      } else { //ios
-        console.log("iOS platform");
-        const name = imagePath.substr(imagePath.lastIndexOf('/') + 1);
-        const path = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
-        console.log('Path from filePath substring: ' + path);
-        console.log('Name from imagePath substring: ' + name);
-        this.copyFileToLocalDir(path, name);
+      return path;
+    }
+
+    public uploadImage() {
+      const fileToUpload = this.pathForImage(this.lastImage);
+      console.log('FilePath for image: ' + fileToUpload);
+
+      const profileData = {
+        filePath: fileToUpload,
+        formData: this.profileData //Pass data from create profile page back to restore state
+      };
+
+      if (this.imageFor == 'user') {
+        this.navCtrl.push('CreateUserProfilePage', profileData);
+      } else {
+        this.navCtrl.push('CreateMatchProfilePage', profileData);
       }
-    }, (err) => console.log(err));
-  }
-
-  private copyFileToLocalDir(namePath, currentName) {
-    const imageFileName = new Date().toUTCString() + '.jpg';
-    this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, imageFileName)
-    .then(() => {
-      console.log('file copied to local directory');
-      this.lastImage = imageFileName;
-    }, err => console.error('ERROR', err));
-  }
-
-  public pathForImage(img) {
-    const path = img === null ? '' : cordova.file.dataDirectory + img;
-    console.log('pathForImage ', path);
-    return path;
-  }
-
-  public uploadImage() {
-    const fileToUpload = this.pathForImage(this.lastImage);
-    console.log('FilePath for image: ' + fileToUpload);
-    console.log('Filename for most recent image: ' + this.lastImage);
-
-    const profileData = {
-      filePath: fileToUpload,
-      fileName: this.lastImage,
-      formData: this.profileData //Pass data from create profile page back to restore state
-    };
-
-    if (this.imageFor == 'user') {
-      this.navCtrl.push('CreateUserProfilePage', profileData);
-    } else {
-      this.navCtrl.push('CreateMatchProfilePage', profileData);
     }
   }
-}
