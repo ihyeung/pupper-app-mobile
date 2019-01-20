61b270f2fc78116dda6d993489b5336100582713
ihyeung <yeung.ireneh@gmail.com>
2019-01-20 08:58:44 -0700

    deleted isProd references

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,10 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-// import { enableProdMode } from '@angular/core';
 import { environment as ENV } from '../environments/environment';
 import { AppModule } from './app.module';
-
-// if (ENV.isProd) {
-//   enableProdMode();
-// }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,5 @@
 export var environment = {
      BASE_URL:"http://localhost:5000",
      AUTO_FILL: false,
-     isProd: false,
      RETRIEVE_WITH_ZIP_DATA: true
 }
