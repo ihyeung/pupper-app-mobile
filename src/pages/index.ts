dc7a62010a387830615cab4b0100a13b98550b7b
ihyeung <yeung.ireneh@gmail.com>
2019-02-11 20:29:03 -0700

    max image bytes for image upload validation logic

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,4 @@ export const DEFAULT_IMG = 'assets/img/logo.png';
 export const DEFAULT_USER_IMG = 'assets/img/default_user.png';
 export const MATCH_PROFILE_ERROR = 'Create a matching profile to begin matching!';
 export const USER_PROFILE_ERROR = 'Create a user profile to get started!';
+export const MAX_IMAGE_BYTES = 1048576;
