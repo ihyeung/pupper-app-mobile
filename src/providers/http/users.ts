b7e95ef303cc706b2332336bee507025ed304bc2
ihyeung <yeung.ireneh@gmail.com>
2019-02-14 20:00:39 -0700

    provider methods for deleting user profile by user profile id and deleting user account by email

--- src/providers/http/users.ts
+++ src/providers/http/users.ts
@@ -9,73 +9,74 @@ export class Users {
   basicHeaders: any;
   authHeaders: any;
 
-  constructor(public http: Http,
-    public utils: StorageUtilities) {
-      this.basicHeaders = new Headers({ 'Content-Type': 'application/json' });
-                                      // ,'Origin': ENV.SERVICE_URL});
-        }
+  constructor(public http: Http, public utils: StorageUtilities) {
+    this.basicHeaders = new Headers({ 'Content-Type': 'application/json' });
+    // ,'Origin': ENV.SERVICE_URL});
+  }
 
-    createUserAccount(username, password) {
-      let signupData = JSON.stringify({
-        username: username,
-        password: password
-      });
-      const registerUrl = `${ENV.BASE_URL}/account/register`;
-      console.log(registerUrl);
+  createUserAccount(username, password) {
+    let signupData = JSON.stringify({
+      username: username,
+      password: password
+    });
+    const registerUrl = `${ENV.BASE_URL}/account/register`;
+    console.log(registerUrl);
 
-      return this.http.post(registerUrl, signupData, { headers: this.basicHeaders });
-    }
+    return this.http.post(registerUrl, signupData, { headers: this.basicHeaders });
+  }
 
-    authenticateUser(username, password) {
-      let loginData = JSON.stringify({
-        username: username,
-        password: password
-      });
+  authenticateUser(username, password) {
+    let loginData = JSON.stringify({
+      username: username,
+      password: password
+    });
 
-      const loginUrl = `${ENV.BASE_URL}/login`;
-      console.log(loginUrl);
-      return this.http.post(loginUrl, loginData, { headers: this.basicHeaders });
-    }
+    const loginUrl = `${ENV.BASE_URL}/login`;
+    console.log(loginUrl);
+    return this.http.post(loginUrl, loginData, { headers: this.basicHeaders });
+  }
 
-    getUserAccountByEmail(username: string, headers: any) {
-      const getUserAccountByEmailUrl = `${ENV.BASE_URL}/account?email=${username}`;
+  getUserAccountByEmail(username: string, headers: any) {
+    const getUserAccountByEmailUrl = `${ENV.BASE_URL}/account?email=${username}`;
 
-      this.authHeaders = headers;
-      console.log(getUserAccountByEmailUrl);
-      return this.http.get(getUserAccountByEmailUrl, { headers: this.authHeaders });
-    }
+    this.authHeaders = headers;
+    console.log(getUserAccountByEmailUrl);
+    return this.http.get(getUserAccountByEmailUrl, { headers: this.authHeaders });
+  }
 
-    updateUserAccount(userAccount: any) {
-      const email = userAccount['username'];
-      const url = `${ENV.BASE_URL}/account?email=${email}`;
-      console.log(url);
-      return this.http.put(url, userAccount, { headers: this.authHeaders });
-    }
+  updateUserAccount(userAccount: any) {
+    const email = userAccount['username'];
+    const url = `${ENV.BASE_URL}/account?email=${email}`;
+    console.log(url);
+    return this.http.put(url, userAccount, { headers: this.authHeaders });
+  }
 
-    getUserProfile(userProfileId: number, headers: any) {
-      const url = `${ENV.BASE_URL}/user/${userProfileId}`;
-      console.log(url);
+  deleteUserAccount(user)
 
-      this.authHeaders = headers;
+  getUserProfile(userProfileId: number, headers: any) {
+    const url = `${ENV.BASE_URL}/user/${userProfileId}`;
+    console.log(url);
 
-      return this.http.get(url, { headers: this.authHeaders });
-    }
+    this.authHeaders = headers;
 
-    getUserProfileByEmail(userEmail: string, headers: any) {
-      const retrieveUserProfileUrl = `${ENV.BASE_URL}/user?email=${userEmail}`;
-      console.log(retrieveUserProfileUrl);
+    return this.http.get(url, { headers: this.authHeaders });
+  }
 
-      this.authHeaders = headers;
-      return this.http.get(retrieveUserProfileUrl, { headers: this.authHeaders });
-    }
+  getUserProfileByEmail(userEmail: string, headers: any) {
+    const retrieveUserProfileUrl = `${ENV.BASE_URL}/user?email=${userEmail}`;
+    console.log(retrieveUserProfileUrl);
 
-    createUserProfile(userProfileObj, authHeaders) {
-      this.authHeaders = authHeaders;
-      const createUserProfileUrl = `${ENV.BASE_URL}/user`;
-      console.log('Creating a new user profile: ' + createUserProfileUrl);
+    this.authHeaders = headers;
+    return this.http.get(retrieveUserProfileUrl, { headers: this.authHeaders });
+  }
 
-      return this.http.post(createUserProfileUrl, userProfileObj,
-          { headers: authHeaders });
+  createUserProfile(userProfileObj, authHeaders) {
+    this.authHeaders = authHeaders;
+    const createUserProfileUrl = `${ENV.BASE_URL}/user`;
+    console.log('Creating a new user profile: ' + createUserProfileUrl);
+
+    return this.http.post(createUserProfileUrl, userProfileObj,
+      { headers: authHeaders });
     }
 
     updateUserProfileById(userProfileObj: any, userId: number) {
@@ -86,23 +87,23 @@ export class Users {
     }
 
     updateLastLogin(userProfileObj: any, date: string) {
-        const updateLastLoginUrlString = ENV.BASE_URL +
-        "/user/" + userProfileObj['id'] + "?lastLogin=" + date;
+      const updateLastLoginUrlString = ENV.BASE_URL +
+      "/user/" + userProfileObj['id'] + "?lastLogin=" + date;
 
-        console.log('updating last login: ' + updateLastLoginUrlString);
-        return this.http.put(updateLastLoginUrlString, userProfileObj,
-          { headers: this.authHeaders });
+      console.log('updating last login: ' + updateLastLoginUrlString);
+      return this.http.put(updateLastLoginUrlString, userProfileObj,
+        { headers: this.authHeaders });
+      }
+
+      deleteUserAccount(email: string) {
+        const url = `${ENV.BASE_URL}/account?email=${email}`;
+        console.log(url);
+        return this.http.delete(url, { headers: this.authHeaders });
+      }
+
+      deleteUserProfile(email: string) {
+        const url = `${ENV.BASE_URL}/user?email=${email}`;
+        return this.http.delete(url, { headers: this.authHeaders });
+      }
+
     }
-
-    deleteUserAccount(email: string) {
-      const url = `${ENV.BASE_URL}/account?email=${email}`;
-      console.log(url);
-      return this.http.delete(url, { headers: this.authHeaders });
-    }
-
-    deleteUserProfile(email: string) {
-      const url = `${ENV.BASE_URL}/user?email=${email}`;
-      return this.http.delete(url, { headers: this.authHeaders });
-    }
-
-}
