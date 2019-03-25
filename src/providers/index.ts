ba7d65047f0ea4b2ae323f728863c72e18c28d4c
ihyeung <yeung.ireneh@gmail.com>
2019-03-25 01:53:28 -0600

    updated reference to match profiles provider

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,6 +1,6 @@
 export { Utilities } from './utility/utils';
 export { StorageUtilities } from './utility/storage';
-export { MatchProfiles } from './http/matchProfiles';
+export { MatchProfiles } from './http/match-profiles';
 export { Users } from './http/users';
 export { Messages } from './http/messages';
 export { Matches } from './http/matches';
