207ed8b5de42df819f792964ec5715cd8f2f5964
ihyeung <yeung.ireneh@gmail.com>
2019-03-25 01:52:53 -0600

    default about me constant

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,4 +2,5 @@ export const DEFAULT_IMG = 'assets/img/logo.png';
 export const DEFAULT_USER_IMG = 'assets/img/default_user.png';
 export const MATCH_PROFILE_ERROR = 'Create a matching profile to begin matching!';
 export const USER_PROFILE_ERROR = 'Create a user profile to get started!';
+export const DEFAULT_ABOUT_ME = 'I love my human and I love all puppers';
 export const MAX_IMAGE_BYTES = 1048576;
