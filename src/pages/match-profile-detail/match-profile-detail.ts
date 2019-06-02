ccbdbd32aecd3176320ad13da6e4121db6fabc96
ihyeung <yeung.ireneh@utah.edu>
2019-06-01 23:52:24 -0600

    modified html for match profile detail page to use ngFor, testing out a bunch of tweaks to match profiel detail page to support displaying either a list of match profiles and a single profile on page

--- src/pages/match-profile-detail/match-profile-detail.ts
+++ src/pages/match-profile-detail/match-profile-detail.ts
@@ -12,17 +12,16 @@ import { DEFAULT_IMG } from '../';
 export class MatchProfileDetailPage {
 
   id: number;
-  profile: any;
+  // profile: any;
   profileReady: boolean = false;
   readOnly: boolean = true;
   matchProfiles: any = []; //Routed from view match profiles button from profile snapshot page, modal will show list of match profiles with active match profile at the top
   matchPreferences: any = [];
-  isDefaultForActiveUser: boolean = false;
+  // isDefaultForActiveUser: boolean = false;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public utils: Utilities, public matchProfileService: MatchProfiles,
-    public storage: StorageUtilities) {
-    }
+    public storage: StorageUtilities) { }
 
     ionViewDidLoad() {
       console.log('ionViewDidLoad MatchProfileDetailPage');
@@ -30,28 +29,31 @@ export class MatchProfileDetailPage {
       //TODO: Display modal pane with match profiles you can swipe through
       //TODO: if matchProfiles nav param is passed, then there should also be a
       // matchProfile param representing the active/default match profile, this should be displayed
-      //at the top of the list of profiles
+      // at the top of the list of profiles
     }
 
     init() {
       this.id = this.navParams.get('matchProfileId');//Only sent from matching page
       this.readOnly = this.navParams.get('readOnly');//Determines whether or not to display edit button
-      this.matchProfiles = this.navParams.get('matchProfiles');//Passed from profile snapshot page
-      const matchProfile = this.navParams.get('matchProfile'); //Passed from inbox page, if passed from profile snapshot page, represents first (ie active/default) match profile to display in list
+      this.matchProfiles = this.navParams.get('matchProfiles');//List passed from profile snapshot page, otherwise list containing single match profile element when coming from settings, matching, or inbox pages
+      // const matchProfile = this.navParams.get('matchProfile'); //Passed from inbox page, if passed from profile snapshot page, represents first (ie active/default) match profile to display in list
 
       if (this.id) {
         this.retrieveProfileData();
       }
-      else if (matchProfile) {
-        this.initProfileData(matchProfile);
+      if (this.matchProfiles) {
+        console.log('User has ' + this.matchProfiles.length + ' match profiles');
+        this.initProfileData();
+
       }
+      // else if (matchProfile) {
+      //   this.initProfileData(matchProfile);
+      // }
       if (this.readOnly !== undefined && !this.readOnly) {
         console.log('not read only, retrieve match preference data');
         this.retrieveMatchPreferences();
       }
-      if (this.matchProfiles) {
-        console.log('User has ' + this.matchProfiles.length + ' match profiles');
-      }
+
     }
 
     retrieveMatchPreferences() {
@@ -72,25 +74,31 @@ export class MatchProfileDetailPage {
       .map(res => res.json())
       .subscribe(response => { //Directly maps to matchProfile object
         console.log('successfully retrieved match profile by id');
-        this.initProfileData(response);
+        this.matchProfiles = response;
+        this.initProfileData();
       }, err => console.error('ERROR: ', JSON.stringify(err)));
     }
 
-    initProfileData(profile: any) {
-      this.profile = profile;
-      this.id = this.profile['id'];
-      this.profile['profileImage'] =
-      this.utils.validateImageUri(this.profile['profileImage'], DEFAULT_IMG);
-      this.isDefaultForActiveUser = this.profile.isDefault;
-      this.profileReady = true;
+    // initProfileData(profile: any) {
+    initProfileData() {
+      // this.profile = profile;
+      // this.id = this.profile['id'];
+      // this.profile['profileImage'] = this.utils.validateImageUri(this.profile['profileImage'], DEFAULT_IMG);
+      // this.isDefaultForActiveUser = this.profile.isDefault;
+      // this.profileReady = true;
+      this.matchProfiles.forEach(each => {
+        each['profileImage'] = this.utils.validateImageUri(each['profileImage'], DEFAULT_IMG);
+        this.profileReady = true;
+      });
+
     }
 
     editMatchProfile(matchProfile: any) {
       console.log("retrieving match profile for edit/update with id = " + matchProfile['id']);
 
       const profileData = {
-        imagePreview: this.profile.profileImage,
-        formData: this.profile,
+        imagePreview: matchProfile['profileImage'],
+        formData: matchProfile,
         isUpdate: true,
         matchPreferenceData: this.buildMatchPreferenceMap()
       };
